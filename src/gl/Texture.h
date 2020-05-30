34efb7689e8d2c1e801ca79c1ba487d9c5c2df5d
Twometer <twometer@outlook.de>
2020-05-30 10:43:28 +0200

    Add basic libraries and stuff

---
+++ src/gl/Texture.h
@@ -0,0 +1,22 @@
+//
+// Created by twome on 06/05/2020.
+//
+
+#ifndef CRYSTALUI_TEXTURE_H
+#define CRYSTALUI_TEXTURE_H
+
+#include <glad/glad.h>
+
+struct Texture {
+
+    GLuint id;
+    unsigned int width;
+    unsigned int height;
+
+    void bind() {
+        glBindTexture(GL_TEXTURE_2D, id);
+    }
+
+};
+
+#endif //CRYSTALUI_TEXTURE_H
---
+++ src/gl/Vao.h
@@ -0,0 +1,45 @@
+//
+// Created by twome on 30/05/2020.
+//
+
+#ifndef CRYSTALUI_VAO_H
+#define CRYSTALUI_VAO_H
+
+
+#include <glad/glad.h>
+
+class Vao {
+private:
+    GLuint vao = 0;
+
+    /**
+     * Contains the vertex positions
+     */
+    GLuint vertexBuffer = 0;
+
+    /**
+     * May contain colors or textures
+     * for the vertices
+     */
+    GLuint metaBuffer = 0;
+
+    int vertexCount = 0;
+    int dimensions = 0;
+
+public:
+    explicit Vao(int dimensions);
+
+    ~Vao();
+
+    void bind() const;
+
+    void unbind() const;
+
+    void set_data(float *vertexBuf, int vertexBufSize, float *metaBuf, int metaBufSize, int metaBufDimensions = 3);
+
+    void render() const;
+
+};
+
+
+#endif //CRYSTALUI_VAO_H
